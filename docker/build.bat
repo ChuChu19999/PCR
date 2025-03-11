a044c973dd09f01070d60f8e0505eda88ed502ee
ChuChu19999 <chert2017ggg@mail.ru>
2025-03-11 12:38:25 +0500

    Изменен docker

--- docker/build.bat
+++ docker/build.bat
@@ -1,16 +1,4 @@
 @echo off
-chcp 1251
-
-:: Включаем BuildKit для ускорения сборки
-set DOCKER_BUILDKIT=1
-set COMPOSE_DOCKER_CLI_BUILD=1
-
-:: Параллельная сборка образов с оптимизированными настройками
-docker compose build ^
-  --parallel ^
-  --build-arg BUILDKIT_INLINE_CACHE=1 ^
-  --progress=plain > build_log.txt 2>&1 ^
-  --no-cache
+docker compose down
+docker compose build --no-cache
 docker compose push
-
-%*
--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-# Включаем BuildKit для ускорения сборки
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
 
-# Параллельная сборка образов с оптимизированными настройками
-docker compose build \
-  --parallel \
-  --build-arg BUILDKIT_INLINE_CACHE=1 \
-  --progress=plain > build_log.txt 2>&1 \
-  --no-cache
+docker compose build --no-cache --progress=plain > build_log.txt 2>&1
 docker compose push
 
 exec "$@"
