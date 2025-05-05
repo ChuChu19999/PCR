073dd44f2a280f2d07663b1645422cd34c34e28d
ChuChu19999 <chert2017ggg@mail.ru>
2025-05-05 08:58:30 +0500

    v10

--- frontend/src/pages/HelpPage/HelpPage.jsx
+++ frontend/src/pages/HelpPage/HelpPage.jsx
@@ -7,6 +7,17 @@ const HelpPage = () => {
     <Layout title="Помощь">
       <HelpPageWrapper>
         <div style={{ paddingTop: '20px' }}>
+          <p className="title">Новости обновлений:</p>
+          <ul className="list">
+            <li className="list-item">
+              на страницу подразделения "26 съезда КПСС" добавлены новые методы исследований: плотность при 20℃ и массовая доля воды
+            </li>
+            <li className="list-item">
+              во вкладке "Протоколы" реализовано прикрепление свободных расчетов и формирование протоколов с прикрепленными расчетами
+            </li>
+          </ul>
+        </div>
+        <div>
           <p className="title">Если у Вас возникли вопросы по работе в системе:</p>
           <ul className="list">
             <li className="list-item">позвоните по номеру телефона 9-66-99</li>
